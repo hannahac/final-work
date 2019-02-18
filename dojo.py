e7aa56bc3c764992d542ea72bf7a61a4e469fc64
hannahac <hannahapio@gmail.com>
2019-02-18 13:32:51 +0300

    add changes to file

--- dojo.py
+++ dojo.py
@@ -1,6 +1,4 @@
-
-
-
+from docopt import docopt
 usage = '''
     Office Space Allocation
 
@@ -8,35 +6,38 @@ usage = '''
 
 
         dojo.py  Office Space Allocation create_room (<room_type> <room_name> ...) 
-        dojo.py  Office Space Allocation add_person (<person_name> <FELLOW|STAFF>[wants_accomodation])
+        dojo.py  Office Space Allocation add_person (<person_name> <FELLOW|STAFF>[--o <wants_accomodation>])
 
 
 '''
-from docopt import docopt
 args = docopt(usage)
+
 
 def output_rooms_created(room_type, room_name):
     if room_type == 'office':
         print('An {} called {} has been created'.format(room_type, room_name))
     else:
-        print('A {} called {} has been created'.format(room_type, room_name))    
+        print('A {} called {} has been created'.format(room_type, room_name))
+
 
 if args['create_room']:
     allowed_room_types = ['office', 'livingspace']
     room_type = args['<room_type>']
     room_name = args['<room_name>']
     if room_type in allowed_room_types == 'office':
-       output_rooms_created(room_type, room_name)
+        output_rooms_created(room_type, room_name)
     elif room_type in allowed_room_types == 'livingspace':
-        output_rooms_created(room_type, room_name)   
+        output_rooms_created(room_type, room_name)
     else:
         print('{} is not a recognised room type'.format(room_type))
+
 
 def people_added(person_name, role):
     if role == 'fellow':
         print('{} {} has been successfuly added'.format(person_name, role))
     else:
         print('{} {} has been successfuly added'.format(person_name, role))
+
 
 if args['add_person']:
     allowed_roles = ['fellow', 'staff']
@@ -45,6 +46,4 @@ if args['add_person']:
     if role != allowed_roles:
         people_added(person_name, role)
     else:
-        print('{} is not a recognised role'.format(role))    
-
-
+        print('{} is not a recognised role'.format(role))
